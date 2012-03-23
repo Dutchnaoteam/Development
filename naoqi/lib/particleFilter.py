adee7ddda7d5c61c11b8c9a5f2feb313b2fbb3bf
Inge <inge.becht91@gmail.com>
2012-03-23 11:32:23 +0100

    fixed a typo in particleFilter.py

--- naoqi/lib/particleFilter.py
+++ naoqi/lib/particleFilter.py
@@ -18,7 +18,7 @@ class ParticleFilter(threading.Thread):
     control = [0,0,0]
     meanState = [0,0,0]
     counter = 0
-    lock = threading.Lock():
+    lock = threading.Lock()
     
     # feature coordinates per id, (x,y).
     # note that the naming is based on naoviewpoint (left poles are
